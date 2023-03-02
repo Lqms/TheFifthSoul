7316bd02d16f83b42f842da6e6f695ea39783cc0
Luqmus <rjcnz1007@yandex.ru>
2023-03-02 12:13:38 +0400

    удалил лишние скрипты
    Я не придумал как сделать взаимодействие с веревкой, а также в диз. доке тоже нет инфы, поетому скрипты по итогу удалил, которые начинал писать

--- Assets/Scripts/InteractableObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class InteractableObject : MonoBehaviour
-{
-    public abstract void Use(Player player);
-}
